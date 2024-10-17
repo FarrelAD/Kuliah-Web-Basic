e8471296510388391b6bbdc6b5c93e845fa4d79f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-10-17 20:00:49 +0700

    add 8th practicum 8th week

---
+++ 08-minggu-8/08/session_create.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Session Create</title>
+</head>
+<body>
+    <?php
+        $_SESSION['favcolor'] = "green";
+        $_SESSION['favanimal'] = "cat";
+        echo "Session variable sudah dibuat!";
+    ?>
+</body>
+</html>
