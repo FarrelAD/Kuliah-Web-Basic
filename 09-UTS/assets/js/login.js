a6c4e6fe278a96ef916a5a990d38d6955a0ca19d
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-10-23 05:46:13 +0700

    fix login redirection if success

--- 09-UTS/assets/js/login.js
+++ 09-UTS/assets/js/login.js
@@ -2,55 +2,24 @@ $(document).ready(() => {
     $('#form-login').submit(function(e) {
         e.preventDefault()
 
-        const nimValue = $('#nim-input').val()
-        const passwordValue = $('#password-input').val()
+        const formData = $(this).serialize()
 
-        if (!isUserInputEmpty(nimValue, passwordValue)) {
-            const formData = $(this).serialize()
-
-            $.ajax({
-                type: 'POST',
-                url: $(this).attr('action'),
-                data: formData,
-                success: (res) => {
-                    // window.location.href = '../pages/member/dashboard.php'
-                },
-                error: (xhr, status, error) => {
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action'),
+            data: formData,
+            dataType: 'json',
+            success: (res) => {
+                if (res.status == 'Success') {
+                    window.location.href = res.redirectionPath
+                } else {
                     alert('Login gagal!')
                 }
-            })
-        }
+            },
+            error: (xhr, status, error) => {
+                alert('Login gagal!')
+            }
+        })
         
     })
 })
-
-
-// CATATAN: Cek lagi fungsi di bawah untuk melakukan validasi
-// sepertinya bisa dilakukan validasi tanpa fungsi di bawah
-function isUserInputEmpty(nimValue, passwordValue) {
-    let isInputEmpty = true
-
-    if (nimValue == '' && passwordValue == '') {
-        alert('Input NIM dan password tidak boleh kosong!')
-    } else if (nimValue == '') {
-        alert('Input NIM tidak boleh kosong!')
-    } else if (passwordValue == '') {
-        alert('Input password tidak boleh kosong!')
-    } else {
-        isInputEmpty = false
-    }
-
-    return isInputEmpty
-}
-
-function isVarNumeric(value) {
-    console.log('fungsi is var numeric terpanggil!')
-    let result = true
-
-    if (typeof value === 'number') {
-        result = false
-        alert('Input NIM harus berupa angka!')
-    }
-
-    return result
-}
--- 09-UTS/controllers/login.php
+++ 09-UTS/controllers/login.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION['candidate_data'])) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nim = trim(htmlspecialchars($_POST['nim']));
     $password = trim(htmlspecialchars($_POST['password']));
+    $is_login_successful = false;
 
     foreach ($_SESSION['users_data'] as $user) {
         if ($user['nim'] == $nim && $user['password'] == $password) {
@@ -24,11 +25,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['name'] = $user['name'];
             $_SESSION['role'] = $user['role'];
 
-            echo "Login sukses!";
-            exit();
+            $is_login_successful = true;
+            break;
         }
     }
 
-    echo "Ada masalah!";
-    // header('Location: ../pages/member/dashboard.php');
+    if ($is_login_successful) {
+        $redirection_path = "";
+        if ($_SESSION['role'] == 'admin') {
+            $redirection_path = "admin/dashboard.php";
+        } elseif ($_SESSION['role'] == 'member') {
+            $redirection_path = "member/dashboard.php";
+        }
+
+        $response = [
+            "status"=> "Success",
+            "redirectionPath"=> $redirection_path,
+        ];
+    }  else{
+        $response = [
+            "status"=> "Failed",
+            "message"=> "Login failed!",
+        ];
+    }
+    
+    echo json_encode( $response );
 }
